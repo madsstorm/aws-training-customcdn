504ce41e7720d84c0fe59cbd57a996b649cb61fc
madsstorm <madsstorm@gmail.com>
2019-06-24 21:30:45 +0200

    Initial function

---
+++ src/Input.Tests/FunctionTest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.TestUtilities;
+
+using Input;
+
+namespace Input.Tests
+{
+    public class InputFunctionTest
+    {
+        [Fact]
+        public void TestToUpperFunction()
+        {
+
+            // Invoke the lambda function and confirm the string was upper cased.
+            var function = new InputFunction();
+            var context = new TestLambdaContext();
+            var upperCase = function.FunctionHandler("hello world", context);
+
+            Assert.Equal("HELLO WORLD", upperCase);
+        }
+    }
+}
