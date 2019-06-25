282d2e236fd6415c28ae1e0cfa63248330c09d95
mads.hansen <mads.hansen@valtech.com>
2019-06-25 10:18:42 +0200

    SQS

--- src/Input/InputFunction.cs
+++ src/Input/InputFunction.cs
@@ -28,16 +28,13 @@ namespace Input
             string email = body.email;
 
             IEnumerable<dynamic> images = body.images;
-            foreach(dynamic image in images)
-            {
-            }
 
-            var amazonSQSClient = new AmazonSQSClient(RegionEndpoint.EUCentral1);
+            IAmazonSQS amazonSQS = new AmazonSQSClient(RegionEndpoint.EUCentral1);
 
             SendMessageRequest sendMessageRequest = new SendMessageRequest { QueueUrl = "https://sqs.eu-central-1.amazonaws.com/265904212570/ImageQueue" };
             sendMessageRequest.MessageBody = "Hej Mads";
 
-            SendMessageResponse sendMessageResponse = amazonSQSClient.SendMessageAsync(sendMessageRequest).Result;
+            SendMessageResponse sendMessageResponse = amazonSQS.SendMessageAsync(sendMessageRequest).Result;
 
             return new APIGatewayProxyResponse { StatusCode = 200, Body = $"OK" };
         }
